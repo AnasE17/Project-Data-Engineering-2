4766cee3eed37ede91fe432eebf6b765a7dec746
AnasE17 <anas_haris98@hotmail.com>
2020-12-17 17:37:17 +0100

    Update test_stress.py

--- test_stress.py
+++ test_stress.py
@@ -10,15 +10,15 @@ except ImportError:
 class TestClassifier(unittest.TestCase):
 
 	
-    
-    #stress test 1000 queries
-	def test_f_stress_test(self):
-		model =load_model()
-		start=time.time()
+	def stress_test(self):
+		start = time.time()
 		for i in range(1000):
-			get_tweet_similar('test')
-		end=time.time()
-		print(end-start)
+			params = {
+				'txt': "test",
+				"form_type": "submit_txt"
+			}
+			rep=requests.post('http://localhost:5000/', data=params)
+		end = time.time()
 		self.assertGreater(60 , end-start)
 
 
