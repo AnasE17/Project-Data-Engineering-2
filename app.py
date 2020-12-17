b3bf00b72a19b68210f5d459ce071ce3565bad05
AnasE17 <anas_haris98@hotmail.com>
2020-12-17 16:05:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,8 @@ def index():
 
 
 if __name__ == '__main__':
+	nltk.download('stopwords')
+	nltk.download('punkt')
+	nltk.download('words')
+	nltk.download('wordnet')
     app.run(host='0.0.0.0', port='5000')
